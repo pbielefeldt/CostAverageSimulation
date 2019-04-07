8f5aad44ff5edb5e82df65b0408cde99f0101e71
Philipp Bielefeldt <p.bielefeldt@hiskp.uni-bonn.de>
2019-04-07 13:53:28 +0200

    fit

--- TotalReturnSim.C
+++ TotalReturnSim.C
@@ -3,10 +3,12 @@
 
 #include "TROOT.h"
 #include "TSystem.h"
+#include "TStyle.h"
 //#include "TApplication.h"
 #include "TRandom.h"
 #include "TRandom2.h"
 #include "TH1D.h"
+#include "TF1.h"
 #include "TCanvas.h"
 
 using namespace std;
@@ -53,14 +55,28 @@ void TotalReturnSim()
   
 //----------------------------------------------------------------------------//
   int runs = 250000;                // number of simulation runs
+  double xmin = 0.;
+  double xmax = 30000.;
   TH1D* result 
-    = new TH1D("result", "distribution of final amount", 1500, 0., 30000.);
+    = new TH1D("result", "distribution of final amount", 1500, xmin, xmax);
   result->GetXaxis()->SetTitle("final amount");
   result->GetYaxis()->SetTitle("prevalence");
+  
+  // exponentially modified gauss function to fit
+  TF1 *gauss_expo 
+    = new TF1("gauss_expo",
+              "[0]*[2]*[3] * TMath::Sqrt(TMath::Pi()/2.) * TMath::Exp( 0.5*[2]*[3]*[2]*[3] - [3]*(x-[1]) ) * TMath::Erfc( ([2]*[3] - (x-[1])/[2])/TMath::Sqrt(2.) )",
+              xmin, xmax
+              );
+  gauss_expo->SetParName(0,"amp");
+  gauss_expo->SetParName(1,"mu");
+  gauss_expo->SetParName(2,"sigma");
+  gauss_expo->SetParName(3,"lambda");
   
   // main loop
   for (int y=0; y<runs; y++)
   {
+      // receive this simulation run's results
       std::vector< std::pair<double,double> > my_sample 
         = sample(0, annual_payment, average_yield/100., yield_sigma/100., number_of_years);
       
@@ -69,8 +85,16 @@ void TotalReturnSim()
       );
   }
   
+  // fit parameters
+  gauss_expo->SetParameters(runs/50., result->GetMean()*0.85, result->GetStdDev()/5., 0.0005);
+  result->Fit("gauss_expo", "ML"); // add "ML" for Minuit-improved log-likelihood
+
+	gStyle->SetOptStat(1101);
+	gStyle->SetOptFit(01111);
+
   TCanvas *c1 = new TCanvas("c1","result",200,10,700,500);
   result->Draw();
+  gauss_expo->Draw("same");
   c1->Update();
   
 }
